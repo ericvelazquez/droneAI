8f90ff352dfcedf083bf5acaedb96abccc47a9bc
Eric <eric-5@hotmail.com>
2018-11-06 17:19:41 -0700

    Added ps_drone API and adding drone movement to the test_Eric.py file

--- research/object_detection/test_Eric.py
+++ research/object_detection/test_Eric.py
@@ -10,6 +10,10 @@ from io import StringIO
 from PIL import Image
 import cv2
 from pathlib import Path
+import time
+import ps_drone
+import argparse
+
 
 sys.path.append("..")
 from utils import label_map_util
@@ -31,15 +35,23 @@ PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
 
 # List of the strings that is used to add correct label for each box.
 PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
-
 NUM_CLASSES = 90
 
+#Using drone bool
+DRONE_ON = False
+
+### Check if drone's flag is ON
+parser = argparse.ArgumentParser()
+parser.add_argument('--drone', action='store_true')
+options = parser.parse_args()
+if options.drone:
+  DRONE_ON = True
+  
 
 ### Download Model
 
 my_file = Path(MODEL_NAME+"/frozen_inference_graph.pb")
 if my_file.is_file() == False: #Element not downloaded yet
-  print("downloading file")
   opener = urllib.request.URLopener()
   opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
   tar_file = tarfile.open(MODEL_FILE)
@@ -47,11 +59,8 @@ if my_file.is_file() == False: #Element not downloaded yet
     file_name = os.path.basename(file.name)
     if 'frozen_inference_graph.pb' in file_name:
       tar_file.extract(file, os.getcwd())
-else:
-  print("File already downloaded")
 
 ### Load a (frozen) Tensorflow model into memory.
-
 detection_graph = tf.Graph()
 with detection_graph.as_default():
   od_graph_def = tf.GraphDef()
@@ -63,15 +72,12 @@ with detection_graph.as_default():
 
 ### Loading label map
 # Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
-
-
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
 
-# ## Helper code
-
+### Helper code
 
 def load_image_into_numpy_array(image):
   (im_width, im_height) = image.size
@@ -93,8 +99,29 @@ def person_counter(classes):
           person_counter += 1
   return "Persons: " + str(person_counter)
 
-# Capture video from webcam
-cap = cv2.VideoCapture(0) 
+def set_up_drone():
+  """
+  Set ups the AR drone 2.0 and return drone object
+  """
+  drone = ps_drone.Drone()                                    # Start using drone
+  drone.startup()                                             # Connects to drone and starts subprocesses
+  drone.reset()                                               # Sets drone's status to good
+  while (drone.getBattery()[0] == -1):      time.sleep(0.1)   # Waits until drone has done its reset
+  print "Battery: "+str(drone.getBattery()[0])+"%  "+str(drone.getBattery()[1]) 
+  drone.useDemoMode(True)                                     # Just give me 15 basic dataset per second
+  drone.setConfigAllID()                                      # Go to multiconfiguration-mode
+  drone.setConfig("control:altitude max","1800")
+  drone.sdVideo()                                             # Choose lower resolution
+  drone.frontCam()                                            # Choose front view
+  return drone
+  
+#If we are using the drone, set it up and use its camera
+if DRONE_ON: 
+  ARdrone = set_up_drone()
+  cap = cv2.VideoCapture('tcp://192.168.1.1:5555')            # Connect to drone camera
+else:
+  # Capture video from webcam
+  cap = cv2.VideoCapture(0) 
 with detection_graph.as_default():
   with tf.Session(graph=detection_graph) as sess:
     while True:
@@ -130,6 +157,36 @@ with detection_graph.as_default():
       cv2.putText(image_np, str_person_counter, (10, 30), font, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
       # Show image
       cv2.imshow('object detection', cv2.resize(image_np, (800,600)))
-      if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
+
+      # Control Drone
+      if DRONE_ON and ARdrone:
+        key = ARdrone.getKey()
+        if key == " ":
+          if ARdrone.NavData["demo"][0][2] and not ARdrone.NavData["demo"][0][3]: 
+            ARdrone.takeoff()
+          else:                               
+            ARdrone.land()
+        elif key == "0":  
+          ARdrone.hover()
+        elif key == "w":  
+          ARdrone.moveForward()
+        elif key == "s":  
+          ARdrone.moveBackward()
+        elif key == "a":  
+          ARdrone.moveLeft()
+        elif key == "d":  
+          ARdrone.moveRight()
+        elif key == "q":  
+          ARdrone.turnLeft()
+        elif key == "e":  
+          ARdrone.turnRight()
+        elif key != "":
+          ARdrone.stop()
+          ARdrone.land()
+          cv2.destroyAllWindows()   
+          break
+
+      else:
+        if cv2.waitKey(25) & 0xFF == ord('v'):
+          cv2.destroyAllWindows()
+          break
